60afb22df35b50fd13adde3536042738e63985f9
Andrzej Kowalski <jedreksmith@poczta.onet.pl>
2024-06-08 13:21:13 +0200

    Add project files.

---
+++ W_2_L_8_10_NWW/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace W_2_L_8_10_NWW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("obliczanie NWW");
+
+           
+
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            int a= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę: ");
+            int b = int.Parse(Console.ReadLine());
+
+
+            int iloczyn = a * b;
+
+            while (a != b)
+            {
+                if (a > b)
+                {
+                    a = a - b;
+                }
+                else
+                {
+                    b = b - a;
+                }
+            }
+
+
+            Console.WriteLine(("Najmniejsza wspolna wielokrotnosc to: ") + iloczyn / a );
+
+        }
+    }
+}
